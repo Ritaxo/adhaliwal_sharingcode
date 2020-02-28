a1d43842c052078cb557bd7536ea3379043d4beb
Ritaxo <adhaliwal@mills.edu>
2020-02-28 14:04:30 -0800

    Module 0 update

---
+++ share/urls.py
@@ -0,0 +1,7 @@
+# Module 0
+from django.urls import path, include
+from . import views  #import everything from views module
+app_name = 'share'
+urlpatterns = [
+    path('',views.index, name='index'),
+]
--- share/views.py
+++ share/views.py
@@ -1,3 +1,9 @@
+# Module 0
 from django.shortcuts import render
+from django.http import HttpResponse
+from share.models import Script
 
 # Create your views here.
+def index(request):
+    if request.method == "GET":
+        return HttpResponse("Hello World!")
